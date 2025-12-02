1abc283a60e392c4623d499769c936995bc36414
Faridul Haque Murshed <faridmurshed9@gmail.com>
2025-12-02 18:40:55 +0600

    health api updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,13 @@ import { EnvironmentConfigModule, TypeOrmConfigModule } from './infrastructure';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
 import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
 
 @Module({
   imports: [
     TypeOrmConfigModule,
+    TypeOrmModule.forFeature([UserEntity]),
     AuthModule,
     ChatModule,
     EnvironmentConfigModule,
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { EnvironmentConfigService } from './infrastructure';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly envConfig: EnvironmentConfigService) {}
-  getHello(): any {
-    return { status: 'ok' };
+  constructor(
+    private readonly envConfig: EnvironmentConfigService,
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+  async getHello() {
+    const user = await this.userRepository.findOne({
+      where: { email: 'faridmurshed11@gmail.com' },
+    });
+
+    return { message: `Hello, ${user?.email}` };
   }
 }
