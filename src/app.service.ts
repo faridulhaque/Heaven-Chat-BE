0f2b0650f36a4b1f1c28104da4f1475f4e4cfcd0
Faridul Haque Murshed <faridmurshed9@gmail.com>
2025-12-04 22:09:19 +0600

    logger added

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { EnvironmentConfigService, ServiceLevelLogger } from './infrastructure';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
@@ -15,11 +15,18 @@ export class AppService {
     private readonly userRepository: Repository<UserEntity>,
   ) {}
   async getHello() {
-    console.log('/health is called');
-    const user = await this.userRepository.findOne({
-      where: { email: 'faridmurshed11@gmail.com' },
-    });
-
-    return { message: `Hello, ${user?.email}` };
+    try {
+      this.logger.verbose('health is called');
+      const user = await this.userRepository.findOne({
+        where: { email: 'faridmurshed11@gmail.com' },
+      });
+      if (user) return { message: `Hello, ${user?.email}` };
+    } catch (error) {
+      this.logger.error(error);
+      throw new HttpException(
+        error?.message || 'Something went wrong',
+        HttpStatus?.NOT_FOUND,
+      );
+    }
   }
 }
